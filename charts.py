b042c24fe61df34bba6dfb221e0ce7c66081e99e
rivea0 <58330360+rivea0@users.noreply.github.com>
2021-10-09 16:11:33 +0300

    Use Figure instead of pyplot for matplotlib to use it in web server

--- charts.py
+++ charts.py
@@ -1,31 +1,39 @@
+import base64
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+from matplotlib.figure import Figure
+from io import BytesIO
+
+
 def create_pie_chart(values):
-    """Generate pie chart for the number of pages 
+    """Generate pie chart for the number of pages
         of the books in user's list."""
-    
+
     # Organize labels according to approximate number of pages
     page_labels = ["Over 500 pages", "~400 pages", "~300 pages", "~200 pages", "~100 pages"]
-    
+
     # Generate colors for the pie chart
     pie_colors = ["#FF0055", "#46F973", "#ED7DED", "#00FFFF", "#FF8000"]
 
-    # Create the chart with percentage values
-    plt.pie(values, colors=pie_colors, shadow=True, autopct="%1.1f%%")
-    
-    # Create legend for the chart
-    plt.legend(labels=page_labels, loc='upper right', bbox_to_anchor=(0.77, 0.55, 0.5, 0.5), fontsize=10, labelspacing=1)
-    
-    # Save the chart image
-    filename = "overview_pie.png"
-    f_path = os.path.join("static/images/", filename)
-    plt.savefig(f_path, bbox_inches='tight', pad_inches=0)
-    plt.close()
+    fig = Figure()
+    ax = fig.subplots()
 
-    return f_path
+    # Create the chart with percentage values
+    ax.pie(values, colors=pie_colors, shadow=True, autopct="%1.1f%%")
+
+    # Create legend for the chart
+    ax.legend(labels=page_labels, loc='upper right', bbox_to_anchor=(0.77, 0.55, 0.5, 0.5), fontsize=10, labelspacing=1)
+
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png", bbox_inches='tight', pad_inches=0)
+
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
 
 
 def create_histogram(time_stats):
@@ -36,7 +44,8 @@ def create_histogram(time_stats):
     books_hours = [time_stats[k] for k in time_stats.keys()]
 
     # Create figure and subplots
-    fig, ax = plt.subplots(1, 1)
+    fig = Figure()
+    ax = fig.subplots(1, 1)
 
     hours = np.array(books_hours)
 
@@ -48,10 +57,10 @@ def create_histogram(time_stats):
     ax.set_xlabel('Ranges of Hours')
     ax.set_ylabel('Number of Books')
 
-    # Save the chart image
-    filename = "overview_histogram.png"
-    f_path = os.path.join("static/images/", filename)
-    plt.savefig(f_path, bbox_inches='tight', pad_inches=0.3)
-    plt.close()
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png", bbox_inches='tight', pad_inches=0.3)
 
-    return f_path
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return data
